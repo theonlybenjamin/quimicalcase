e2332a68f10f1858f012dbe0a8a8cd4f48a62e3a
= <benja.villajuan@gmail.com>
2022-01-26 23:10:17 -0500

    agregando modal de sen dpending

--- src/app/components/send-pending-item/send-pending-item.component.ts
+++ src/app/components/send-pending-item/send-pending-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SendPending } from 'src/app/interfaces/send-pending';
 import { FirebaseService } from 'src/app/services/firebase.service';
 
@@ -12,12 +13,22 @@ export class SendPendingItemComponent implements OnInit {
   @Input() summaryText: string = '';
   @Input() orders: SendPending[] = [];
   @Input() showButton: boolean = true;
-  constructor(public fireService: FirebaseService) { }
+  public selectedOrder: SendPending = {} as SendPending;
+  @ViewChild('successModal') successModal: ElementRef | undefined;;
+  constructor(
+    public fireService: FirebaseService,
+    private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
 
   public completeOrder(order: SendPending) {
-    this.fireService.deleteItemPendingCollection(order);
+    this.selectedOrder = order;
+    this.modalService.open(this.successModal, {centered: true});
+  }
+
+  public confirmCompleteOrder() {
+    this.fireService.deleteItemPendingCollection(this.selectedOrder);
+    this.modalService.dismissAll();
   }
 }
