c3b801a6ccc52ec4e91d72571cfc549604e879ef
= <benja.villajuan@gmail.com>
2022-02-08 16:40:25 -0500

    agregando finanzas

--- src/app/components/send-pending-item/send-pending-item.component.ts
+++ src/app/components/send-pending-item/send-pending-item.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SendPending } from 'src/app/interfaces/send-pending';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import Decimal from 'decimal.js';
 
 @Component({
   selector: 'app-send-pending-item',
@@ -14,12 +15,23 @@ export class SendPendingItemComponent implements OnInit {
   @Input() orders: SendPending[] = [];
   @Input() showButton: boolean = true;
   public selectedOrder: SendPending = {} as SendPending;
-  @ViewChild('successModal') successModal: ElementRef | undefined;;
+  @ViewChild('successModal') successModal: ElementRef | undefined;
+  public packagingPrice: number = 5;
+  public advertisingPrice: number = 3.5;
+  public totalSummary: number = 0;
+  public salary: number = 0;
+  public reinvesment: number = 0;
+  public advertisement: number = 0;
   constructor(
     public fireService: FirebaseService,
     private modalService: NgbModal) { }
 
   ngOnInit(): void {
+    this.orders.forEach(x => this.totalSummary += (x.total? x.total : 0));
+    this.salary = Number(new Decimal(this.totalSummary * 0.35).toPrecision(4));
+    this.reinvesment = Number(new Decimal(this.totalSummary * 0.50).toPrecision(4));
+    this.advertisement = Number(new Decimal(this.totalSummary * 0.15).toPrecision(4));
+
   }
 
   public completeOrder(order: SendPending) {
@@ -30,5 +42,15 @@ export class SendPendingItemComponent implements OnInit {
   public confirmCompleteOrder() {
     this.fireService.deleteItemPendingCollection(this.selectedOrder);
     this.modalService.dismissAll();
+  }
+
+  public getExpensesSummary(order: SendPending): number {
+    return (order.costo_delivery? order.costo_delivery : 0) + (order.capital? order.capital : 0) + this.packagingPrice + this.advertisingPrice;
+  }
+
+  public getProfit(order: SendPending): number {
+    const total = new Decimal(order.total? order.total : 0);
+    const expenses = new Decimal(this.getExpensesSummary(order))
+    return total.minus(expenses).toNumber();
   }
 }
