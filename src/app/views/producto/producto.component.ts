330629e4a7a92789f2d93b02116a09725a882ce2
= <benja.villajuan@gmail.com>
2022-04-25 19:55:07 -0500

    terminando catalogo de cases

--- src/app/views/producto/producto.component.ts
+++ src/app/views/producto/producto.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Product } from 'src/app/interfaces/stock';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { idByIphoneName } from 'src/app/utils/utils';
@@ -9,22 +10,26 @@ import { idByIphoneName } from 'src/app/utils/utils';
   templateUrl: './producto.component.html',
   styleUrls: ['./producto.component.scss']
 })
-export class ProductoComponent implements OnInit {
+export class ProductoComponent implements OnInit, OnDestroy {
 
   public iphoneCode: string = '';
   public justStock: Array<Product> = []
+  private subscriptions = new Subscription();
   constructor(
     private route: ActivatedRoute,
     private fireService: FirebaseService
   ) {
     const ga = this.route.snapshot.queryParamMap.get('id');
     this.iphoneCode = idByIphoneName(ga ? ga.replace("-", " ") : 'error');
-    this.fireService.getStockSpecificDocumentAlone(this.iphoneCode).subscribe(x => {
+    this.subscriptions.add(this.fireService.getStockSpecificDocumentAlone(this.iphoneCode).subscribe(x => {
       this.justStock = x.data;
-    });
+    }));
   }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
